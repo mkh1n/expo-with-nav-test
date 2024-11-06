70428b81b2453e4d46c5907d6eec07f7f610d206
Roman <romamuhino1@gmai.com>
2024-11-06 22:56:53 +0300

    add notfound screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,11 +7,11 @@ import { ThemedView } from '@/components/ThemedView';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: 'Уууупс!!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+        <ThemedText type="title">Таакой страницы не существует.</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link">Иди домой!</ThemedText>
         </Link>
       </ThemedView>
     </>
